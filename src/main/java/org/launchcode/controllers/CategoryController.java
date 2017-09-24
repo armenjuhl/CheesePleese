83794a4d814484b09be48aeb1d10e7ad50c941bf
armenjuhl <armenjuhl@gmail.com>
2017-09-24 15:44:13 -0700

    Step two finished

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -9,9 +9,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import javax.validation.Valid;
-import javax.validation.constraints.Null;
 
 
 @Controller
@@ -41,6 +39,7 @@ public class CategoryController {
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public String add(Model model,
       @ModelAttribute @Valid Category category, Errors errors) {
+
         if (errors == null) {
             model.addAttribute(categoryDao.save(category));
             return "redirect:";
--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,10 +1,10 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Category {
@@ -21,6 +21,10 @@ public class Category {
 
     public void Category(String aName){ setName(aName); }
 
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
+
 
     public int getId() {
         return id;
@@ -31,5 +35,6 @@ public class Category {
     public String getName() {
         return name;
     }
+
     }
 
